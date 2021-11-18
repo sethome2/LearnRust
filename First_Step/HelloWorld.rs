49d67b4faa45fd51494ff25d86e1cffd40b796f4
sethome2 <N/A>
2021-11-19 00:03:00 +0800

    First rs without cargo

---
+++ First_Step/HelloWorld.rs
@@ -0,0 +1,17 @@
+/*
+ * @Author: sethome
+ * @Date: 2021-11-18 23:36:40
+ * @LastEditTime: 2021-11-18 23:56:35
+ * @FilePath: \LearnRust\First_Step\HellWorld.rs
+ */
+
+fn printMain()
+{
+    println!("Hello, world!\n");    
+    println!("Hello, world!\n");
+}
+
+fn main() 
+{
+    printMain();
+}
