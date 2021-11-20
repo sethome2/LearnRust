42014de6fa88845a50fc4c2c3f278745d8457cb9
sethome2 <N/A>
2021-11-21 01:30:57 +0800

    more try

--- First_Step/First project/first_project/src/main.rs
+++ First_Step/First project/first_project/src/main.rs
@@ -1,3 +1,11 @@
+use std::io;
+
 fn main() {
     println!("Hello, world!");
+    let mut a = String::new();
+    
+    io::stdin().read_line(&mut a)
+        .expect("Failed to read line");
+
+    println!("You guessed: {}", a);
 }
